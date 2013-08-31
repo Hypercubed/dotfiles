da1d046fa977ec07973c9a32f27ff291ecfaf0b4
J. Harshbarger <hypercubed@gmail.com>
2013-08-31 23:20:17 +0900

    Added vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@ ln -s ~/.dotfiles/bashrc ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/freshrc ~/.freshrc
 ln -s ~/.dotfiles/liquidpromtrc ~/.liquidpromtrc
+ln -s ~/.dotfiles/vimrc ~/.vimrc
