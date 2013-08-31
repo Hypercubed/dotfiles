c863ea1f7362b96384a324fa8c16d462624a63f4
J. Harshbarger <hypercubed@gmail.com>
2013-08-31 23:20:39 +0900

    Sublime text 3

--- cygwin/notepad.sh
+++ cygwin/notepad.sh
@@ -1,15 +1,14 @@
-export NP="${PF:=`cygpath "$PROGRAMFILES"`}/Notepad++/notepad++.exe"
-export SUPL="${PF:=`cygpath "$PROGRAMFILES"`}/Sublime Text 2/sublime_text.exe"
+export SUPL="/cygdrive/c/Program Files/Sublime Text 3/sublime_text.exe"
 export VISUAL=`cygpath -w "$SUPL"`
-export EDITOR="$NP"
+export EDITOR="vim"
 
-git config --global --replace-all core.editor "vim"
+git config --global --replace-all core.editor $EDITOR
 
 function np() {
 	if [ "$1" == "" ] ; then
 		cygstart "$SUPL"
 	else
-		cygstart "$SUPL" `winpath "$1"`
+		cygstart "$SUPL" `winpath $@`
 	fi
 }
 
@@ -17,17 +16,17 @@ function supl() {
 	if [ "$1" == "" ] ; then
 		cygstart "$SUPL"
 	else
-		cygstart "$SUPL" `winpath "$1"`
+		cygstart "$SUPL" `winpath $@`
 	fi
 }
 
 function editor() {
 case "$1" in
 	*_EDITMSG|*MERGE_MSG|*_TAGMSG )
-		vim "$1"
+		$EDITOR $@
 		;;
 	* )
-		np "$1"
+		supl $@
 		;;
 esac
 }
