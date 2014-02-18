b42002bc6f34351fb0553ce15c21aa14e7180a6a
Jayson Harshbarger <hypercubed@gmail.com>
2014-02-18 11:33:35 +0900

    Moved some alias to cygwin to avoid issues on macos

---
+++ cygwin/alias.sh
@@ -0,0 +1,5 @@
+# Aliases
+#
+# Some shortcuts for different directory listings (this don't work on mac)
+alias ls='ls -hF --color=tty'                 # classify files in colour
+alias dir='ls --color=auto --format=vertical'
